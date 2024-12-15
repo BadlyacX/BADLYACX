e4c971c7b11b921d0b0f52f47755129c77ea3596
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2024-12-15 23:02:16 +0800

    Update a001.java

--- ZeroJudge/src/main/java/practice/a001.java
+++ ZeroJudge/src/main/java/practice/a001.java
@@ -1,4 +1,12 @@
 package practice;
 
+import java.util.Scanner;
+
 public class a001 {
+    static Scanner scanner = new Scanner(System.in);
+    static String input = scanner.nextLine();
+
+    public static void main(String[] args) {
+        System.out.println("hello, " + input);
+    }
 }
