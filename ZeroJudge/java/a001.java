756f4b17adab66f154c2c5378aa18632989531e4
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2024-12-17 01:15:31 +0800

    Add files via upload

---
+++ ZeroJudge/java/a001.java
@@ -0,0 +1,11 @@
+package practice;
+
+import java.util.Scanner;
+
+public class a001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("hello, " + input);
+    }
+}
---
+++ ZeroJudge/python/a002.py
@@ -0,0 +1,3 @@
+number = input()
+num1, num2 = map(int, number.split( ))
+print(num1 + num2)
