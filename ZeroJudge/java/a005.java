92e1af0bdd9355418925fc0d9c3f4397d7df00c3
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2025-03-17 11:57:17 +0800

    Update a005.java

--- ZeroJudge/java/a005.java
+++ ZeroJudge/java/a005.java
@@ -1,1 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class a005 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        int lines = scanner.nextInt();
+        if (lines > 20 || lines < 0) {
+            System.out.println("err lines");
+            scanner.close();
+            return;
+        }
+        for (int i = 1; i <= lines; i++) {
+            String input = scanner.nextLine();
+            input
+        }
+            
+        scanner.close();      
+    }
+}
