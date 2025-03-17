cc9c0ee781b1790ee4a2b1e81cafafe48a08ced1
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2025-03-17 18:06:19 +0800

    Update a005.java

--- ZeroJudge/java/a005.java
+++ ZeroJudge/java/a005.java
@@ -1,37 +1,34 @@
+package ZeroJudge;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class a005 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-      
+        ArrayList<Integer> list = new ArrayList<>();
+
         int lines = scanner.nextInt();
-        if (lines > 20 || lines < 0) {
-            System.out.println("err lines");
-            scanner.close();
-            return;
-        }
         for (int i = 1; i <= lines; i++) {
-            int[] arr = new int[3];
-            for (int j = 0; j <= 3; j++) {
-                arr[j] = scanner.nextInt();
-            }
+    
+
             int fifth;
-            if (arr[1] - arr[0] == arr[2] - arr[1]) {
-                int diff = arr[1] - arr[0];
-                fifth = arr[3] + diff;
+
+            if (list.get(1) - list.get(0) == list.get(2) - list.get(1)) {
+                int diff = list.get(1) - list.get(0);
+                fifth = list.get(3) + diff;
             } else {
-                int ratio = arr[1] / arr[0];
-                fifth = arr[3] * ratio;
+                int ratio = list.get(1) / list.get(0);
+                fifth = list.get(3) * ratio;
             }
 
             for (int j = 0; j <= 4; j++) {
-              System.out.print(arr[j] + " ");
+                System.out.print(list.get(j) + " ");
             }
             System.out.println(fifth);
-          
+
         }
 
-        scanner.close();      
+        scanner.close();
     }
 }
