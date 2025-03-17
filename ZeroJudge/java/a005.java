2a53cb2e676cc2bd87fdbf2246f1fcba98171175
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2025-03-17 16:27:36 +0800

    Update a005.java

--- ZeroJudge/java/a005.java
+++ ZeroJudge/java/a005.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class a005 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
+      
         int lines = scanner.nextInt();
         if (lines > 20 || lines < 0) {
             System.out.println("err lines");
@@ -12,10 +12,26 @@ public class a005 {
             return;
         }
         for (int i = 1; i <= lines; i++) {
-            String input = scanner.nextLine();
-            input
+            int[] arr = new int[3];
+            for (int j = 0; j <= 3; j++) {
+                arr[j] = scanner.nextInt();
+            }
+            int fifth;
+            if (arr[1] - arr[0] == arr[2] - arr[1]) {
+                int diff = arr[1] - arr[0];
+                fifth = arr[3] + diff;
+            } else {
+                int ratio = arr[1] / arr[0];
+                fifth = arr[3] * ratio;
+            }
+
+            for (int j = 0; j <= 4; j++) {
+              System.out.print(arr[j] + " ");
+            }
+            System.out.println(fifth);
+          
         }
-            
+
         scanner.close();      
     }
 }
