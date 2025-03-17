da2fda679a9f04140f39eb0d9c7b009707dafe74
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2025-03-17 19:21:42 +0800

    Update a005.java

--- ZeroJudge/java/a005.java
+++ ZeroJudge/java/a005.java
@@ -1,34 +1,44 @@
 package ZeroJudge;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class a005 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
-        int lines = scanner.nextInt();
-        for (int i = 1; i <= lines; i++) {
-    
-
-            int fifth;
-
-            if (list.get(1) - list.get(0) == list.get(2) - list.get(1)) {
-                int diff = list.get(1) - list.get(0);
-                fifth = list.get(3) + diff;
-            } else {
-                int ratio = list.get(1) / list.get(0);
-                fifth = list.get(3) * ratio;
-            }
-
-            for (int j = 0; j <= 4; j++) {
-                System.out.print(list.get(j) + " ");
-            }
-            System.out.println(fifth);
-
+        int t = sc.nextInt();
+        sc.nextLine();
+        int a = 0;
+    while (sc.hasNext()) {
+        if (a == t) {
+            break;
         }
 
-        scanner.close();
+        String line = sc.nextLine();
+        String[] strings = line.split("\\s+");
+        int[] numbers = new int[strings.length];
+
+        for (int i = 0; i < strings.length; i++) {
+            numbers[i] = Integer.parseInt(strings[i]);
+        }
+
+        int fifth;
+        if (numbers[1] - numbers[0] == numbers[2] - numbers[1]) {
+            int diff = numbers[1] - numbers[0];
+            fifth = numbers[3] + diff;
+        } else {
+            int ratio = numbers[1] / numbers[0];
+            fifth = numbers[3] * ratio;
+        }
+
+        for (int i = 0; i <= 3; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println(fifth);
+
+        a++;
+    }
+
+        sc.close();
     }
 }
