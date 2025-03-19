cdd162410c51e5ed1eda4d454dfba5c4f071856a
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2025-03-19 18:13:35 +0800

    Create a009.java

---
+++ ZeroJudge/java/a009.java
@@ -0,0 +1,21 @@
+package ZeroJudge;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class a009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        int k = 7;
+        String input = sc.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            int n = (int) c;
+            char o = (char)(n - 7);
+            System.out.print(o);
+        }
+        sc.close();
+    }
+}
